c0c50501d2986627c1b095257c12d741ca3f819f
CondesnsedMilk <achau103@student.hbuhsd.edu>
2024-12-19 21:16:07 +0000

    Problem 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace unit_4_4_assignment_CondesnsedMilk;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool done = false;
+
+        while (done != true)
+        {
+            Console.WriteLine("Pick a random number from 1-100");
+            float numPick = Convert.ToSingle(Console.ReadLine());
+
+            if (numPick < 100 && numPick > 1)
+            {
+                Console.WriteLine("Thanks for your input!");
+                done = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid. Please try again.");
+            }
+        }
+
+    }
+}
